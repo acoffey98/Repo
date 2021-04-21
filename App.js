ff5ad68a3ae0d15a69c3183aa6a0545f07a85e92
Alexandria Marie <78360546+acoffey98@users.noreply.github.com>
2021-04-21 12:41:16 -0230

    Add files via upload

--- App.js
+++ App.js
@@ -5,16 +5,29 @@ import Header from "./Header"
 import MainContent from "./MainContent"
 import TodoItem from "./TodoItem"
 import ContactCard from "./ContactCard"
-import Joke from "./Joke"
-import JokesData from "./JokesData"
+
+import productsData from "./vschoolProducts"
+import Product from "./Product"
+import todosData from "./todosData"
+
 
 function App(){
+	const productComponents = productsData.map(item => <Product key={item.id} product={item} />)     
+	//productsData will create a new array
+	//new variable called productComponents and coming from productsData.map()
+	//Creates a new array and fill with productComponent 
+	//Array is filled with productComponents 
+
+	const todoItems = todosData.map(item => <TodoItem key={item.id}item={item} />)
 	
-	return(
+		return(
 		<div>
 			<Header />	
 			<MainContent />
-			<TodoItem />
+
+				{productComponents}
+
+				{todoItems}
 
 			<ContactCard 
 				name="Mr. Jax" 
@@ -41,27 +54,55 @@ function App(){
 				phone="(212)-338-2566"
 				email="mykitty@gmail.com"
 			/>
-			<Joke 
-				question="What is the best thing about Newfoundland?" 
-				punchLine="The scenary"
-			/>
-			<Joke 
-				question="What is your favouirte show?" 
-				punchLine= "Wentworth"
-			/>
-			<Joke 
-				question="What is your last name?" 
-				punchLine="Coffey"
-			/>
-			<Joke 
-				question="What is the capital of Newfoundland?" 
-				punchLine="St.John's"
-			/>
-
 
 			<Footer />
 		</div>
 	)
 }
+
+
+// class App extends React.Component{
+// 	render(){
+// 		return(
+// 		<div>
+// 			<Header />	
+// 			<MainContent />
+
+// 				{productComponents}
+
+// 				{todoItems}
+
+// 			<ContactCard 
+// 				name="Mr. Jax" 
+// 				imgUrl="https://placekitten.com/300/200"
+// 				phone="(212)-338-2455"
+// 				email="mykitty44@hotmail.com"
+	
+// 			/>
+// 			<ContactCard 
+// 				name="Mr. Jax" 
+// 				imgUrl="https://placekitten.com/300/200"
+// 				phone="(212)-338-2455"
+// 				email="mykitty44@hotmail.com"
+// 			/>
+// 			<ContactCard 
+// 				name="Mr. KittyKat" 
+// 				imgUrl="https://placekitten.com/300/200"
+// 				phone="(212)-338-2882"
+// 				email="mykitty5323@hotmail.com"	
+// 			/>
+// 			<ContactCard 
+// 				name="Mr.Cat" 
+// 				imgUrl="https://placekitten.com/300/200" 
+// 				phone="(212)-338-2566"
+// 				email="mykitty@gmail.com"
+// 			/>
+
+// 			<Footer />
+// 		</div>
+// 	)
+// }
+// }
+
 
 export default App
